5fb760ae34e8760dca7685144903fc5070b1e919
Giacomo Cappon <cappongiacomo@gmail.com>
2020-11-26 13:18:42 +0100

    [master] solves #14.

---
+++ src/processing/imputeGlucose.m
@@ -0,0 +1,53 @@
+function dataImputed = imputeGlucose(data, maxGap)
+%imputeGlucose function that imputes missing glucose data using linear 
+%interpolation. The function imputes only missing data gaps of maximum
+%`maxGap` minutes. Gaps longer than `maxGap` minutes are ignored.
+%
+%Inputs:
+%   - data: a timetable with column `Time` and `glucose` containing the 
+%   glucose data to retime (in mg/dl);
+%   - maxGap: an integer defining the maximum interpol-able missing data 
+%   gaps (in min). 
+%Output:
+%   - dataImputed: the retimed timetable.
+%
+%Preconditions:
+%   - data must be a timetable having an homogeneous time grid;
+%   - maxGap must be an integer.
+%
+% ---------------------------------------------------------------------
+%
+% REFERENCE:
+%   - None.
+%
+% ---------------------------------------------------------------------
+%
+% Copyright (C) 2020 Giacomo Cappon
+%
+% This file is part of AGATA.
+%
+% ---------------------------------------------------------------------
+
+    %Check preconditions 
+    if(~istimetable(data))
+        error('dataImputed: data must be a timetable.');
+    end
+    if(var(seconds(diff(data.Time))) > 0)
+        error('dataImputed: data must have a homogeneous time grid.')
+    end
+    if( ~( isnumeric(maxGap) && ((maxGap - round(maxGap)) == 0) ) )
+        error('dataImputed: timestep must be an integer.')
+    end
+    
+    %Get the sample time
+    sampleTime = minutes(data.Time(2)-data.Time(1));
+    
+    %Find the interpolable gaps 
+    [shortNan, longNan, nanStart, nanEnd] = findNanIslands(data,round(maxGap/sampleTime));
+    
+    %Impute data
+    dataImputed = data;
+    dataImputed.glucose(shortNan) = interp1(data.Time(~isnan(data.glucose)),data.glucose(~isnan(data.glucose)),data.Time(shortNan));
+
+end
+
---
+++ test/processing/imputeGlucoseTest.m
@@ -0,0 +1,48 @@
+% Test units of the imputeGlucose function
+%
+% ---------------------------------------------------------------------
+%
+% Copyright (C) 2020 Giacomo Cappon
+%
+% This file is part of AGATA.
+%
+% ---------------------------------------------------------------------
+
+addpath(fullfile('..','..','src','processing'));
+
+time = datetime(2000,1,1,0,0,0):minutes(5):datetime(2000,1,1,0,0,0)+minutes(120);
+data = timetable(zeros(length(time),1),'VariableNames', {'glucose'}, 'RowTimes', time);
+data.glucose(:) = 120;
+data.glucose(2:3) = nan;
+data.glucose(10:20) = nan;
+data.glucose(22) = nan;
+
+%% Test 1: check the length of results
+results = imputeGlucose(data, 15);
+assert(height(data) == height(results));
+
+%% Test 2: check the interpolated data
+results = imputeGlucose(data, 15);
+assert(results.glucose(2) == 120);
+assert(results.glucose(3) == 120);
+assert(results.glucose(22) == 120);
+
+%% Test 3: check the non interpolated data
+results = imputeGlucose(data, 15);
+assert(all(isnan(data.glucose(10:20))));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
